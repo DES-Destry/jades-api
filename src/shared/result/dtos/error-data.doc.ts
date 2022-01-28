6e8be14dc3c5ca3ce005eaa7f04375721fe5c249
DES-Destry <agafonovandrej69@gmail.com>
2022-01-28 20:22:03 +0600

    fix: error data api properties describer for swagger

--- src/shared/result/dtos/error-data.doc.ts
+++ src/shared/result/dtos/error-data.doc.ts
@@ -1,6 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { ErrorDataDto } from './error-data.dto';
 
 export class ErrorDataDoc {
+  @ApiProperty()
   data: ErrorDataDto;
+
+  @ApiProperty({
+    description: 'True if request was successful.',
+    example: true,
+  })
   isOk: boolean;
 }
--- src/shared/result/dtos/error-data.dto.ts
+++ src/shared/result/dtos/error-data.dto.ts
@@ -1,4 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class ErrorDataDto {
+  @ApiProperty({
+    description:
+      'Message of the error. If you catches 500 it send err.message of try-catch block',
+    example: 'User input wrong - validation error',
+  })
   message: string;
+  @ApiProperty({
+    description: 'Validation errors. It generates if response code was 400.',
+    example: [
+      "It's only for examples in 400 error case",
+      "Password can't be less than 8 symbols",
+      "Username can't be a email",
+    ],
+    type: [String],
+  })
   validationErrors?: string[];
 }
