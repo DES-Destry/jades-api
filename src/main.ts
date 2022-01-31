0102f9ac0a667f67439a5a8c5044bb9823a64b53
DES-Destry <agafonovandrej69@gmail.com>
2022-01-31 22:40:26 +0600

    feat: add global exception filter

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AppConfig } from './shared/config/app.config';
+import { GlobalExceptionFilter } from './shared/filters/global-exception.filter';
 import { BetaKeyGuard } from './shared/guards/beta-key.guard';
 
 async function bootstrap() {
@@ -9,6 +10,7 @@ async function bootstrap() {
 
   app.setGlobalPrefix('/api');
   app.useGlobalGuards(new BetaKeyGuard());
+  app.useGlobalFilters(new GlobalExceptionFilter());
 
   const config = new DocumentBuilder()
     .setTitle('Unimaster Blog API')
