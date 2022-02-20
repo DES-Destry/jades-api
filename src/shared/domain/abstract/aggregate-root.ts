b319be5d70ad47794386103e5cf5595201356c60
DES-Destry <agafonovandrej69@gmail.com>
2022-02-20 17:05:31 +0600

    fix: aggregate root mapping in constructor

--- src/shared/domain/abstract/aggregate-root.ts
+++ src/shared/domain/abstract/aggregate-root.ts
@@ -2,8 +2,9 @@ export abstract class AggregateRoot<T> {
   protected props: T;
 
   constructor(props: T) {
-    for (const field in props) {
-      this.props[field] = props[field];
-    }
+    // for (const field in props) {
+    //   this.props[field] = props[field];
+    // }
+    this.props = props;
   }
 }
