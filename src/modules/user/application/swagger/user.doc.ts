f3170ac56e93f6522b11cbaaa34624585fcf0700
Andrey Kirik <dev.andrey.kirik@gmail.com>
2022-05-23 10:15:12 +0600

    refactor: move swagger documentstion from domain interface to application

---
+++ src/modules/user/application/swagger/user.doc.ts
@@ -0,0 +1,155 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { UserGender } from 'src/shared/domain/common/user-gender';
+import { UserScope } from 'src/shared/domain/common/user-interests';
+import { IUser } from '../../domain/user.interface';
+import { Credentials } from '../../domain/value-objects/credentials.vo';
+import { Location } from '../../domain/value-objects/location.vo';
+
+export abstract class LocationDoc {
+  @ApiProperty({
+    description: 'Country of user.',
+    example: 'USA',
+  })
+  country: string;
+
+  @ApiProperty({
+    description: 'State of user.',
+    example: 'California',
+    required: false,
+  })
+  state?: string;
+
+  @ApiProperty({
+    description: 'City of user',
+    example: 'Los-Angeles',
+    required: false,
+  })
+  city?: string;
+}
+
+export abstract class UserDocData implements IUser {
+  credentials: Credentials;
+  @ApiProperty({
+    description: 'ID of user. It is a UUID v4.',
+    example: '610ccd5e-fc43-42a7-80c6-d9561872c213',
+  })
+  id?: string;
+
+  @ApiProperty({
+    description: 'Username. Used for enter to the system and finding.',
+    example: 'Destry.Unimaster',
+  })
+  username: string;
+
+  @ApiProperty({
+    description:
+      "Alias - alternative username. Used only for showing and don't used for enter to the system or finding.",
+    example: 'DES-Destry',
+  })
+  alias?: string;
+
+  @ApiProperty({
+    description: 'Url alias. Can used for finding instead of username.',
+    example: 'destry_unimaster',
+  })
+  urlAlias?: string;
+
+  @ApiProperty({
+    description:
+      "User's description. User can type there anything stuff like about his skills or his life.",
+    example:
+      "My name is Alan. And I'am a senior Java developer. I've a wife. Help me please.",
+  })
+  description?: string;
+
+  @ApiProperty({
+    description: 'User gender. I must to describe it? Yes, there only two.',
+    example: UserGender.Male,
+    enum: UserGender,
+  })
+  gender: UserGender;
+
+  @ApiProperty({
+    description:
+      'Emails of the user. There have 1 primary mail. Any mails can be a hidden or not.',
+    type: [UserEmailDoc],
+  })
+  emails: IUserEmail[];
+
+  @ApiProperty({
+    description:
+      "Karma earns by user with likes on posts or comments. All positive user's actions.",
+    example: 400,
+  })
+  karma: number;
+
+  @ApiProperty({
+    description: 'Location of the user. User can write where are he from.',
+    type: LocationDoc,
+    required: false,
+  })
+  location?: Location;
+
+  @ApiProperty({
+    description:
+      'ID of the role. By role user have different permissions to ban/delete users/posts/comments.',
+    example: '610ccd5e-fc43-42a7-80c6-d9561872c215',
+  })
+  roleId?: string;
+
+  @ApiProperty({
+    description: 'Role object with additional data.',
+    type: UserRoleDoc,
+  })
+  role?: IUserRole;
+
+  @ApiProperty({
+    description: "User's links to his other accounts",
+    type: [UserContactDoc],
+  })
+  contacts: IUserContact[];
+
+  @ApiProperty({
+    description: "Scope of user's work.",
+    example: UserScope.DevOps,
+    enum: UserScope,
+  })
+  scope: UserScope;
+
+  @ApiProperty({
+    description: 'Company where user works.',
+    example: 'Google Inc.',
+  })
+  company?: string;
+
+  @ApiProperty({
+    description:
+      'Date of last password changing. Used to generate user payload, therefore if this value will changed - access token automatically become not valid.',
+    example: new Date(),
+  })
+  lastPasswordChangedAt: Date;
+
+  @ApiProperty({
+    description: "Date of user's creating. Register date.",
+    example: new Date(),
+  })
+  createdAt?: Date;
+
+  @ApiProperty({
+    description:
+      "Date of user's fields changing. Changes when any parameters of user was changed.",
+    example: new Date(),
+  })
+  updatedAt?: Date;
+}
+
+export class UserDoc {
+  @ApiProperty()
+  data: UserDocData;
+
+  @ApiProperty({
+    description: 'True if request was successful.',
+    example: true,
+  })
+  isOk: boolean;
+}
